3ba919a19f0b06cee059fb6e5937537f2a182666
Kamesh Sampath <kameshsampath@users.noreply.github.com>
2017-08-24 09:15:31 -0400

    Fixing circuit breakers

--- src/main/java/com/redhat/developers/osio/microspringboot1/Microspringboot1Application.java
+++ src/main/java/com/redhat/developers/osio/microspringboot1/Microspringboot1Application.java
@@ -2,11 +2,26 @@ package com.redhat.developers.osio.microspringboot1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableCircuitBreaker
 public class Microspringboot1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Microspringboot1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Microspringboot1Application.class, args);
+    }
+
+    @org.springframework.context.annotation.Configuration
+    public class Configuration {
+
+        @Bean
+        public RestTemplate restTemplate() {
+            return new RestTemplate();
+        }
+    }
 }
