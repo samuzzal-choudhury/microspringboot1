e36d2ad429029b74fa27a0fc0a37eaa78daeccea
Kamesh Sampath <kameshsampath@users.noreply.github.com>
2017-08-29 12:00:16 +0530

    Simplified, removed feign

--- src/main/java/com/redhat/developers/osio/microspringboot1/TheRESTController.java
+++ src/main/java/com/redhat/developers/osio/microspringboot1/TheRESTController.java
@@ -1,32 +1,56 @@
 package com.redhat.developers.osio.microspringboot1;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
-
 public class TheRESTController {
 
-
-    @Autowired
     RestTemplate restTemplate;
 
-    @Autowired
-    CustomersService customersService;
+    @Value("${microspringboot1.customer_url}")
+    String customerServiceUrl;
 
+    @Autowired
+    public TheRESTController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @RequestMapping("/")
     @HystrixCommand(fallbackMethod = "topLevelFallback")
     public String getCustomersWithOrders() {
         System.out.println("getCustomers()");
-        String customers = customersService.getCustomers();
+
+        final URI customerUri = UriComponentsBuilder
+            .fromHttpUrl(customerServiceUrl)
+            .path("/api/customer")
+            .build()
+            .toUri();
+
+
+        System.out.println("Customer Uri " + customerUri.toString());
+
+        String customers = restTemplate.getForObject(customerUri, String.class);
+
         System.out.println(String.format("Got Customers: %s", customers));
 
-        String customerOrders = customersService.getCustomerOrders(customers);
+        final URI customerOrdersUri = UriComponentsBuilder
+            .fromHttpUrl(customerServiceUrl)
+            .path("/api/customer/orders")
+            .build()
+            .toUri();
+
+        System.out.println("Customer Order Uri " + customerOrdersUri.toString());
+
+        String customerOrders = restTemplate.postForObject(customerOrdersUri, customers, String.class);
+
         System.out.println(String.format("Got Customers Orders: %s", customerOrders));
 
         return customerOrders;
